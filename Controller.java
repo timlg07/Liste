42bae4a2a12674a8e7a8a7ebe90769193d83bb83
T!M Greller <timlg07@gmail.com>
2019-05-15 20:17:36 +0200

    [Einfügen:Ende] Controller/Main

--- Controller.java
+++ Controller.java
@@ -1,5 +1,8 @@
 public class Controller {
 	public static void main( String[]tanga ) {
-        
+		Liste l = new Liste(  );
+		l.endeEinfuegen( new Datenelement( 2 ) );
+		l.endeEinfuegen( new Datenelement( 3 ) );
+		l.anfangEinfuegen( new Datenelement( 1 ) );
 	}
 }
