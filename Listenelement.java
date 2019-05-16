3389805282f40a2d3e20179f6eacd104d30caa3e
T!M Greller <timlg07@gmail.com>
2019-05-16 13:25:39 +0200

    [toString/Ausgeben] Listenelement

--- Listenelement.java
+++ Listenelement.java
@@ -2,4 +2,5 @@ public abstract class Listenelement {
     public Listenelement(  ){}
 	public abstract Listenelement endeEinfuegen( Datenelement daten );
 	public abstract int gibLaenge(  );
+	public abstract String toString(  );
 }
