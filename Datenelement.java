ac47ec3ea3d2d878f60ed1c801f2d05d2d939752
T!M Greller <timlg07@gmail.com>
2019-05-18 17:52:18 +0200

    Datenelement: vergleichsfunktion hinzugefügt

--- Datenelement.java
+++ Datenelement.java
@@ -9,4 +9,8 @@ public class Datenelement {
     public String toString(  ) {
     	return "(d:"+this.beispielDaten+")";
     }
+
+    public boolean vergleichen( Datenelement vergleichsObjekt ) {
+    	return this.beispielDaten == vergleichsObjekt.beispielDaten;
+    }
 }
