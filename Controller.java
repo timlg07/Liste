c3a365cd8296bfb84b88a212c4adae784bc6073d
T!M Greller <timlg07@gmail.com>
2019-05-19 13:16:04 +0200

    verbesserte Test-Klasse

--- Controller.java
+++ Controller.java
@@ -1,10 +1,28 @@
 public class Controller {
+	
 	public static void main( String[]tanga ) {
 		Liste l = new Liste(  );
-		l.endeEinfuegen( new Datenelement( 2 ) );
+		// Liste füllen
+		Datenelement d = new Datenelement( 2 );
+		l.endeEinfuegen( d );
 		l.endeEinfuegen( new Datenelement( 3 ) );
 		l.anfangEinfuegen( new Datenelement( 1 ) );
-		System.out.println( l.toString() );
-		System.out.println( l.gibLaenge() );
+		l.endeEinfuegen( new Datenelement( 2 ) );
+		
+		ListeAusgeben( l );
+		
+		// alle (d:2) werden entfernt
+		l.entfernen( d );
+		
+		ListeAusgeben( l );
+		
+		// Das erste Element wird aus der Liste entfernt und ausgegeben
+		System.out.println( l.anfangEntfernen().toString() );
+		
+		ListeAusgeben( l );
+	}
+	
+	public static void ListeAusgeben( Liste l ) {
+		System.out.println( "Länge:"+l.gibLaenge()+" |  Aufbau: "+l.toString() );
 	}
 }
